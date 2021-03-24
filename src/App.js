d741619e97c863fbcf96a51e61e1f66c7ff1b964
vg4838 <vg4838@g.rit.edu>
2021-03-24 12:16:15 -0400

    added header component and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import Header from './components/header/Header';
+
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">
         <h1>Setup react redux</h1>
       </div>
