baa75e02aa868776b8ebf096498a279e84d0fd0f
vg4838 <vg4838@g.rit.edu>
2021-03-27 01:44:06 -0400

    added circle ci test job

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,10 +1,11 @@
+  
 import React from 'react';
 
 import './Spinner.scss';
 
 const Spinner = () => {
   return (
-    <div className="spinner">
+    <div className="spinner" data-testid="spinner">
       <div className="bounce1"></div>
       <div className="bounce2"></div>
       <div className="bounce3"></div>
